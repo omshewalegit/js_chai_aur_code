114043170738781e8baf76c0053c3e3e895714eb
omshewalegit <omshewale0007@gmail.com>
2025-04-13 17:47:33 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accoundId = 1223;
+let accoutEmail = "omshewale23@gmai.com";
+var accountPassword = "12345";
+accoutCity = "Jaipur";
+
+// accoundId = 1246; Not allowed!!!
+
+// prefer not to use var because of issue in block scope and funcitonal scope
+
+accoutEmail = "oms2@gamil.com";
+accountPassword = "321";
+accoutCity = "Mumbai";
+
+// console.log(accoundId);
+console.table([accoundId, accoutEmail, accountPassword, accoutCity]);
