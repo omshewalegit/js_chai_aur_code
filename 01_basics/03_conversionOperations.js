3903f865001dd0dd051d791a35ca45ce76a2371b
omshewalegit <omshewale0007@gmail.com>
2025-04-15 16:34:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = "33abc";
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let number  = 1;
+let isLoggedIn = Boolean(number);
+console.log(isLoggedIn);
+
+//1->"true",0->"false"
